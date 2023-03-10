0c49f23c6bb44b93d5257fff0f7d67d7d19ae6fe
leush <leushkinml@mail.ru>
2023-03-10 17:41:06 +0300

    10.03.2023   17:40

--- src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -1,9 +1,24 @@
+//package pro.sky.telegrambot.service;
+//
+//import pro.sky.telegrambot.model.NotificationTask;
+//
+//import java.util.Optional;
+//import java.util.function.Consumer;
+//
+//public interface NotificationTaskService {
+//    public NotificationTask scheduleTask(Long chatId, NotificationTask task);
+//    public Optional<NotificationTask> parseToNotificationTask(String unformattedTask);
+//
+//    void notifyAllScheduledTasks(Consumer<NotificationTask> notifier);
+//}
+
+
 package pro.sky.telegrambot.service;
 
-import pro.sky.telegrambot.model.NotificationTask;
+        import pro.sky.telegrambot.model.NotificationTask;
 
-import java.util.Optional;
-import java.util.function.Consumer;
+        import java.util.Optional;
+        import java.util.function.Consumer;
 
 public interface NotificationTaskService {
     public NotificationTask scheduleTask(Long chatId, NotificationTask task);
