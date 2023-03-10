2e42f361d5c9a2268fe523a95bc631e516cbea26
leush <leushkinml@mail.ru>
2023-03-10 18:00:17 +0300

    10.03.2023   17:40

--- src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -1,9 +1,9 @@
 package pro.sky.telegrambot.service;
 
-        import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.model.NotificationTask;
 
-        import java.util.Optional;
-        import java.util.function.Consumer;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 public interface NotificationTaskService {
     public NotificationTask scheduleTask(Long chatId, NotificationTask task);
